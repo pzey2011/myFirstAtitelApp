4b23e98c1f276c729fa606e52a220c10d9f4ddcc
pzey2011 <peymanzeynali@yahoo.com>
2016-09-26 16:06:52 +0330

    weather service added for getting JSON from Yahoo!

--- src/main/java/com/example/asus/myfirstatitelapp/service/YahooWeatherService.java
+++ src/main/java/com/example/asus/myfirstatitelapp/service/YahooWeatherService.java
@@ -1,8 +1,38 @@
 package com.example.asus.myfirstatitelapp.service;
 
+import android.os.AsyncTask;
+
 /**
  * Created by asus on 9/26/2016.
  */
 public class YahooWeatherService {
+    private WeatherServiceCallback callback;
+    private String location;
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
+    public String getLocation() {
+        return location;
+    }
+
+
+
+    public YahooWeatherService(WeatherServiceCallback callback) {
+        this.callback = callback;
+    }
+    public void updateWeather(String location)
+    {
+        new AsyncTask<String, Void, String>() {
+
+            @Override
+            protected String doInBackground(String... strings) {
+                return null;
+            }
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+            }
+        }.execute(location);
+    }
 }
